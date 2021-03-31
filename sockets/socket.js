f2f2223f8e3367c8c5d70d04a67ec727b7d3d872
Eduardo Rios Corlay <rios0021@algonquinlive.com>
2021-03-31 18:19:18 -0400

    Changes for bands

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,15 +1,47 @@
 const {io} = require('../index');
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand( new Band('Queen'));
+bands.addBand( new Band('Metallica'));
+bands.addBand( new Band('BonJovi'));
+bands.addBand( new Band('Skap'));
+
 //Socket messages
 io.on('connection', client => {
+
+    client.emit('active-bands', bands.getBands());
 
     console.log('Client connected');
     client.on('disconnect', () => {
         console.log('Client disconected');
     });
-    client.on('message', data => {
-        console.log(data);
+    // client.on('message', payload => {
+    //     console.log(payload);
+    //     io.emit('message', {admin:'New message'});
+    // });
 
-        io.emit('message', {admin:'New message'});
+    // client.on('emit-message', (payload) => {
+    //     client.broadcast.emit('new-message', payload);
+    // }); 
+
+    client.on('connect_failed', (err) => {
+        console.log('Sorry, there seems to be an issue with the connection! $err');
+    });
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+    client.on('add-band', (payload) => {
+        bands.addBand( new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+    });
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
     });
 });
 
